84c5db976e98b72d00111f9c1527549d0baf1f48
tmurray19 <me@taidghmurray.ie>
2019-05-21 19:42:40 +0100

    Unit Testing with Jasmine.

--- Exercise/webapp/spec/server.spec.js
+++ Exercise/webapp/spec/server.spec.js
@@ -2,4 +2,8 @@ describe('calc', () => {
     it('should multiply 2 and 2', () => {
         expect(2*2).toBe(4)
     })
+
+    it('should multiply 2 and 2', () => {
+        expect(2*2).toBe(5)
+    })
 })
